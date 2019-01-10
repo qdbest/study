d0ac81ff7accb270d302c087d46b95dc4aecaa58
yucheng <jmguoyucheng@126.com>
2019-01-11 06:38:05 +0800

    新建eureka

---
+++ sell/eurika/src/test/java/com/yucn/eurika/EurikaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yucn.eurika;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurikaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
